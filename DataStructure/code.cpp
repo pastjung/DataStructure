77fd395e5ceb164af23c5620d1925c136124f9b7
정재현 <chwogus0303@naver.com>
2024-05-18 12:59:20 +0900

    Add ( DataStructure ) : 실행 프로젝트 Add ( 배열 ) Add ( 연결 리스트 )
    - 초기 세팅

---
+++ DataStructure/code.cpp
@@ -0,0 +1,15 @@
+// A file that can be checked by executing the code.
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	// Improve the input/output speed of cin, cout
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	cout << "Test" << "\n";
+
+	return 0;
+}
