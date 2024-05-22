4645ed214970f61a96662e392cc2a99d09ed6f41
정재현 <chwogus0303@naver.com>
2024-05-23 03:15:09 +0900

    add(Hash Table) : 해시 테이블 문제 답안

--- 5. 해시 테이블 ( Hash Table )/1-1.cpp
+++ 5. 해시 테이블 ( Hash Table )/1-1.cpp
@@ -1,13 +1,112 @@
 #include <iostream>
 using namespace std;
 
+#define MAX 353333
+
+class Item {
+public:
+	int key, value;
+};
+
+class arr {
+public:
+	Item* data;
+	bool flag = false;
+};
+
+arr HashArr[MAX];
+int sz = 0;
+
+int hashfunc(int idx) {
+	return idx % MAX;
+}
+
+void tableInsert(int key, int value) {
+	if (sz == MAX) return;
+
+	Item* newItem = new Item();
+	newItem->key = key;
+	newItem->value = value;
+
+	int index = hashfunc(key);
+	while (sz != MAX) {
+		if (HashArr[index].flag == false) {
+			HashArr[index].flag = true;
+			HashArr[index].data = newItem;
+			sz++;
+			break;
+		}
+		else {
+			index = (index + 1) % MAX;
+		}
+	}
+}
+
+void tableSearch(int key, int value) {
+	int index = hashfunc(key);
+	int probing = 1;
+	do {
+		Item* item = HashArr[index].data;
+		bool flag = HashArr[index].flag;
+
+		if (!flag) {
+			cout << 0 << " " << probing << "\n";
+			break;
+		}
+		else if (item->key == key) {
+			if (item->value == value) {
+				cout << 1 << " " << probing << "\n";
+				break;
+			}
+			else {
+				index = hashfunc(++index);
+				probing++;
+			}
+		}
+		else {
+			index = hashfunc(++index);
+			probing++;
+		}
+
+	} while (probing != MAX + 1);
+}
+
+void tableClear() {
+	for (int i = 0; i < MAX; i++) {
+		Item* deleteItem = HashArr[i].data;
+		HashArr[i].data = NULL;
+		HashArr[i].flag = false;
+		delete deleteItem;
+	}
+	sz = 0;
+}
+
 int main() {
 	// Improve the input/output speed of cin, cout
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
 	cout.tie(NULL);
 
-	cout << "Test" << "\n";
+	int T;
+	cin >> T;
+	while (T--) {
+		int N;
+		cin >> N;
+		for (int i = 0; i < N; i++) {
+			int X;
+			cin >> X;
+			tableInsert(hashfunc(X), X);
+		}
 
+		int M;
+		cin >> M;
+		for (int i = 0; i < M; i++) {
+			int X;
+			cin >> X;
+			tableSearch(hashfunc(X), X);
+		}
+
+		tableClear();
+	}
 	return 0;
 }
--- 5. 해시 테이블 ( Hash Table )/2-1.cpp
+++ 5. 해시 테이블 ( Hash Table )/2-1.cpp
@@ -1,13 +1,121 @@
 #include <iostream>
 using namespace std;
 
+#define MAX 353333
+
+class Item {
+public:
+	int key, value;
+};
+
+class arr {
+public:
+	Item* data;
+	bool flag = false;
+};
+
+arr HashArr[MAX];
+int sz = 0;
+
+int hashfunc(int idx) {
+	return idx % MAX;
+}
+
+int hashfunc2(int idx) {
+	return (17 - (idx % 17));
+}
+
+void tableInsertDouble(int key, int value) {
+	if (sz == MAX) return;
+
+	Item* newItem = new Item();
+	newItem->key = key;
+	newItem->value = value;
+
+	int j = 0;
+	while (sz != MAX) {
+		int index = hashfunc(value + j * hashfunc2(value));
+
+		if (HashArr[index].flag == false) {
+			HashArr[index].flag = true;
+			HashArr[index].data = newItem;
+			sz++;
+			break;
+		}
+		else {
+			j++;
+		}
+	}
+}
+
+void tableSearchDouble(int key, int value) {
+	int probing = 1;
+	int j = 0;
+
+	do {
+		int index = hashfunc(value + j * hashfunc2(value));
+
+		Item* item = HashArr[index].data;
+		bool flag = HashArr[index].flag;
+
+		if (!flag) {
+			cout << 0 << " " << probing << "\n";
+			break;
+		}
+		else if (item->key == key) {
+			if (item->value == value) {
+				cout << 1 << " " << probing << "\n";
+				break;
+			}
+			else {
+				j++;
+				probing++;
+			}
+		}
+		else {
+			j++;
+			probing++;
+		}
+
+	} while (probing != MAX + 1);
+}
+
+void tableClear() {
+	for (int i = 0; i < MAX; i++) {
+		Item* deleteItem = HashArr[i].data;
+		HashArr[i].data = NULL;
+		HashArr[i].flag = false;
+		delete deleteItem;
+	}
+	sz = 0;
+}
+
 int main() {
 	// Improve the input/output speed of cin, cout
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
 	cout.tie(NULL);
 
-	cout << "Test" << "\n";
+	int T;
+	cin >> T;
+	while (T--) {
+		int N;
+		cin >> N;
+		for (int i = 0; i < N; i++) {
+			int X;
+			cin >> X;
+			tableInsertDouble(hashfunc(X), X);
+		}
 
+		int M;
+		cin >> M;
+		for (int i = 0; i < M; i++) {
+			int X;
+			cin >> X;
+			tableSearchDouble(hashfunc(X), X);
+		}
+
+		tableClear();
+	}
 	return 0;
 }
